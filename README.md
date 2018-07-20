e1254900ac20b68c253c1ac691a02c41d473ad6b
Minkyu Lee <niklaus@Minkyuui-MacBook-Pro.local>
2018-07-20 15:17:46 +0900

    Add command for firmware update using ymodem protocol #4

--- README.md
+++ README.md
@@ -8,15 +8,40 @@ $ npm install -g kameleon-cli
 
 ## Usage
 
-```bsh
+### Help
+
+```sh
+$ kameleon -h
+```
+
+
+### Flash .js source code
+
+```sh
 $ kameleon flash <file> -p <port>
 ```
 
 * `<file>` : Path to the file to copy.
-* `-p, --port <port>` : Port name where Kameleon is connected. (e.g. `-p /dev/tty.usbmodem1441`)
+* `-p, --port <port>` : Port where device is connected. (e.g. `-p /dev/tty.usbmodem1441`)
 
 __Example__:
 
-```bsh
+```sh
 $ kameleon flash index.js -p /dev/tty.usbmodem1441
+```
+
+### Firmware update
+
+
+```sh
+$ kameleon firmware <file> -p <port>
+```
+
+* `<file>` : Path to the firmware to update.
+* `-p, --port <port>` : Port where device is connected. (e.g. `-p /dev/tty.usbmodem1441`)
+
+__Example__:
+
+```sh
+$ kameleon firmware kameleon.bin -p /dev/tty.usbmodem1441
 ```
--- bin/kameleon.js
+++ bin/kameleon.js
@@ -1,23 +1,36 @@
 #!/usr/bin/env node
 
-var program = require('commander');
-var protocol = require('../lib/protocol');
-var path = require('path');
+var program = require('commander')
+var protocol = require('../lib/protocol')
+// var path = require('path')
 
 program
-  .version('0.1.0');
+  .version('0.1.0')
 
 program
   .command('flash <file>')
   .description('Flash a file to Kameleon')
-  .option("-p, --port <port>", "Port where the file is transmitted")
+  .option('-p, --port <port>', 'Port where device is connected')
   .action(function (file, options) {
-    console.log('Flashing a file...');
-    var base = path.basename(file);
-    var port = options.port;
-    protocol.flash(port, file, function (err) {
-      console.log(`Flashed successfully: ${file}.`);
-    });
-  });
+    console.log('Flashing a file...')
+    // var base = path.basename(file)
+    var port = options.port
+    protocol.flash(port, file, err => {
+      if (err) {
+        console.error(err)
+      } else {
+        console.log(`Flashed successfully: ${file}.`)
+      }
+    })
+  })
 
-program.parse(process.argv);
+program
+  .command('firmware <file>')
+  .description('Update firmware to Kameleon')
+  .option('-p, --port <port>', 'Port where device is connected')
+  .action(function (file, options) {
+    var port = options.port
+    protocol.firmware(port, file)
+  })
+
+program.parse(process.argv)

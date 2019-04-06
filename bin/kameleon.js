b10d7a865f5de649ff86117dca24cd9e6d2321bf
niklauslee <niklaus.lee@gmail.com>
2019-04-06 14:03:14 +0900

    protocol updates and remove system-sleep module

--- bin/kameleon.js
+++ bin/kameleon.js
@@ -22,14 +22,30 @@ program
   .option('-p, --port <port>', 'port where device is connected')
   .action(function (file, options) {
     var port = options.port
-    const serial = new SerialPort(port, serialOptions)
     var code = fs.readFileSync(file, 'utf8')
     console.log('Writing ' + path.basename(file) + '...')
-    protocol.write(serial, code, (err, result) => {
+    const serial = new SerialPort(port, serialOptions)
+    serial.open(err => {
       if (err) {
         console.error(err)
       } else {
-        console.log(`Done. (${file} : ${result.writtenBytes} bytes)`)
+        protocol.write(serial, code, (err, result) => {
+          if (err) {
+            console.error(err)
+          } else {
+            setTimeout(() => {
+              // load written code
+              serial.write('\r\r\r')
+              serial.write('.load\r')
+              setTimeout(() => {
+                if (serial.isOpen) {
+                  serial.close()
+                }
+                console.log(`Done. (${file} : ${result.writtenBytes} bytes)`)
+              }, 1000)
+            }, 1000)
+          }
+        })
       }
     })
   })
@@ -40,13 +56,15 @@ program
   .option('-p, --port <port>', 'port where device is connected')
   .action(function (options) {
     var port = options.port
-    const serial = new SerialPort(port, serialOptions)
     console.log('Erasing user code...')
-    protocol.erase(serial, err => {
+    const serial = new SerialPort(port, serialOptions)
+    serial.open(err => {
       if (err) {
         console.error(err)
       } else {
-        console.log('Done.')
+        protocol.erase(serial, () => {
+          console.log('Done.')
+        })
       }
     })
   })
@@ -58,34 +76,52 @@ program
   .action(function (firmware, options) {
     var port = options.port
     const serial = new SerialPort(port, serialOptions)
-    protocol.send(serial, '.firmup', function (err) {
+    serial.open(err => {
       if (err) {
         console.error(err)
       } else {
-        // Need long time (3sec) to re-identify serial port after disconnection
-        setTimeout(() => {
-          console.log('Updating firmware...')
-          var checkPoint = 1024 * 32
-          var buffer = fs.readFileSync(firmware)
-          const serial2 = new SerialPort(port, serialOptions)
-          protocol.update(serial2, buffer, (err, result) => {
-            if (err) {
-              console.error(err)
-            } else {
-              console.log(`Firmware is successfully updated (${firmware} : ${result.writtenBytes} bytes)`)
-            }
-          }, function (progress) {
-            if (progress.writtenBytes >= checkPoint) {
-              var kb = Math.floor(progress.writtenBytes / 1024)
-              var percent = Math.floor((progress.writtenBytes / progress.totalBytes) * 100)
-              console.log(kb + 'KB uploaded (' + percent + '%)')
-              checkPoint = checkPoint + 1024 * 32
-              if (checkPoint > progress.totalBytes) {
-                checkPoint = progress.totalBytes
+        protocol.send(serial, '.firmup', () => {
+          // Serial port should be closed because the board is rebooted in bootloader mode
+          if (serial.isOpen) {
+            serial.close()
+          }
+          // Need long time (3sec) to re-identify serial port after disconnection
+          setTimeout(() => {
+            console.log('Updating firmware...')
+            var checkPoint = 1024 * 32
+            var buffer = fs.readFileSync(firmware)
+            const serial2 = new SerialPort(port, serialOptions)
+            serial2.open(err => {
+              if (err) {
+                console.error(err)
+              } else {
+                protocol.update(serial2, buffer, (err, result) => {
+                  if (err) {
+                    console.error(err)
+                  } else {
+                    console.log(`Firmware is successfully updated (${firmware} : ${result.writtenBytes} bytes)`)
+                    // Close the serial after firmware update complete
+                    setTimeout(() => {
+                      if (serial2.isOpen) {
+                        serial2.close()
+                      }
+                    }, 500)
+                  }
+                }, function (progress) {
+                  if (progress.writtenBytes >= checkPoint) {
+                    var kb = Math.floor(progress.writtenBytes / 1024)
+                    var percent = Math.floor((progress.writtenBytes / progress.totalBytes) * 100)
+                    console.log(kb + 'KB uploaded (' + percent + '%)')
+                    checkPoint = checkPoint + 1024 * 32
+                    if (checkPoint > progress.totalBytes) {
+                      checkPoint = progress.totalBytes
+                    }
+                  }
+                })
               }
-            }
-          })
-        }, 3000)
+            })
+          }, 3000)
+        })
       }
     })
   })
